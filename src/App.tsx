97651a8818ffb4e38e13cb66e007d6311f62d379
patrickcordeiroestudos <75341307+patrickcordeiro@users.noreply.github.com>
2023-03-02 12:13:55 -0300

    feat: Criando projeto base e adicionando bibliotecas

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import GlobalStyle from "./style/GlobalStyle";
+
+function App() {
+  return (
+    <div className="App">
+      <GlobalStyle />
+    </div>
+  );
+}
+
+export default App;
