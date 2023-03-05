0885fbb2a6e77adf02e73231c03ef3bfa14e2549
patrickcordeiroestudos <75341307+patrickcordeiro@users.noreply.github.com>
2023-03-05 13:38:31 -0300

    feat: Criação de componente Header + Navbar Mobile e Desktop

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import { Header } from "./components/Header";
 import GlobalStyle from "./style/GlobalStyle";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,83 @@
+import styled from "styled-components";
+
+const HeaderComponent = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+`;
+
+const NavbarDesktop = styled.div`
+  width: 100%;
+  display: flex;
+  padding: 10px;
+
+  .navbar {
+    width: 100%;
+    display: flex;
+    align-items: center;
+
+    .container-fluid {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+
+      .navbar-brand {
+        display: flex;
+        align-items: center;
+        gap: 10px;
+        color: black;
+        text-transform: uppercase;
+        font-family: "Roboto", sans-serif;
+        font-weight: bold;
+      }
+
+      .navbar-nav {
+        display: flex;
+        flex-direction: row;
+        gap: 10px;
+
+        .nav-link {
+          font-family: "Roboto", sans-serif;
+          color: black;
+        }
+      }
+    }
+  }
+
+  @media only screen and (max-width: 768px) {
+    display: none;
+  }
+`;
+
+const NavbarMobile = styled.div`
+  width: 100%;
+
+  .navbar {
+    width: 100%;
+
+    .container-fluid {
+      .navbar-brand {
+        display: flex;
+        align-items: center;
+        gap: 10px;
+        color: black;
+        text-transform: uppercase;
+        font-family: "Roboto", sans-serif;
+        font-weight: bold;
+      }
+    }
+  }
+
+  .logo {
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+  }
+
+  @media only screen and (min-width: 768px) {
+    display: none;
+  }
+`;
+
+export { HeaderComponent, NavbarDesktop, NavbarMobile };
